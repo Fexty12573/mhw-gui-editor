dbb5efe7689938abc63ee5c8b6d93cfb7b053f2c
Fexty12573 <bogofogo1@gmail.com>
2023-01-14 17:58:05 +0100

    Some more changes to project + main file

---
+++ mhw-gui-editor/Main.cpp
@@ -0,0 +1,9 @@
+
+#include <Windows.h>
+
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	MessageBox(nullptr, "Hello World!", "Hello World!", MB_OK);
+	return 0;
+}
