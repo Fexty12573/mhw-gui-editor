86a5393aea64aa65cb69cad0147f56b7844adff0
Fexty12573 <bogofogo1@gmail.com>
2023-08-06 18:15:37 +0200

    Disable console in release builds

--- mhw-gui-editor/Main.cpp
+++ mhw-gui-editor/Main.cpp
@@ -5,14 +5,19 @@
 #include <spdlog/spdlog.h>
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+#if defined(_DEBUG)
     const auto& console = Console::get();
 	console->initialize();
 
     const auto log = console->get_logger();
+#endif
 
 	App app;
 	const int result = app.run();
+#if defined(_DEBUG)
+    log->info("App::run() returned {}", result);
 	console->shutdown();
+#endif
 
 	return result;
 }
